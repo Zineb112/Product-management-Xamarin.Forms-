94178b3f163cf7885cf6acb2b9504353c7bd343b
Zineb Belassiri <57940406+Zineb112@users.noreply.github.com>
2021-06-02 07:39:37 +0000

    update

---
+++ Product management/Product management/Product management/Dependencies/IDataBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_management.Dependencies
+{
+    interface IDataBase
+    {
+        SQLite.SQLiteConnection GetConnection();
+    }
+}
